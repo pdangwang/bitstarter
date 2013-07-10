c8944a658905a986e7a38005fb664c5b3056ac0e
Ping Dang <pdangwang@yahoo.com>
2013-07-10 09:51:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+var fs=require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+//var buf= fs.readFileSync("index.html", "utf-8");
+//var contents = contents_buffer.toString();
+//console.log(contents);
+
+  response.send("hello workld 3!");
+
 });
 
 var port = process.env.PORT || 5000;
