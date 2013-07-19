0a317e04b919dc724273d4c35b59f701fae38cfd
Ubuntu <ubuntu@ip-172-31-47-172.us-west-2.compute.internal>
2013-07-19 01:06:11 +0000

    please commit

--- web.js
+++ web.js
@@ -1,20 +1,16 @@
-var fs=require('fs');
-
 var express = require('express');
+var fs = require('fs');
+
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-var buf= fs.readFileSync("index.html", "utf-8");
-var contents = buf.toString();
-//console.log(contents);
-
-  response.send(contents);
-
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
